e402485a5440091e41b434f76aba88513316eea9
lkssmnt <hello@lsiemoneit.de>
2025-11-12 09:40:00 +0100

    added header and footer snippets

---
+++ site/snippets/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?= css("assets/css/styles.css") ?>
+  <title><?= $site->title() ?> | <?= $page->title() ?></title>
+</head>
+
+<body>
